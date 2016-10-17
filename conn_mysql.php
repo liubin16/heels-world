d1c6aa0b5b69a9539ad14e4fe80ad8f0d11bf6be
liubinrobin <liubinrobin@didichuxing.com>
2016-10-17 13:14:20 +0800

    edit connect mysql file

--- conn_mysql.php
+++ conn_mysql.php
@@ -1,3 +1,16 @@
 <?php
+	$mysql_server_name = 'localhost';
+	$mysql_username = 'root';
+	$mysql_password = '123456';
+	$mysql_database = 'test_db';
 
+	$conn = mysql_connect($mysql_server_name, $mysql_username, $mysql_password);
+	mysql_query("set names 'utf8'");
+	mysql_select_db($mysql_database);
+	$sql = "select * from test_tbl ";
+	$result = mysql_query($sql, $conn);
+
+	while ($row = mysql_fetch_array($result)) {
+		echo $row['name']."<br/>";
+	}
 ?>
